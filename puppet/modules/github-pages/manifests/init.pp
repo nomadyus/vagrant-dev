7e83889c090819c379ab886276ea1f4d4129e6f7
yusuf-kami <yusuf.kami@gmail.com>
2016-04-21 20:13:20 -0400

    adding  github-pages module

--- puppet/modules/github-pages/manifests/init.pp
+++ puppet/modules/github-pages/manifests/init.pp
@@ -1,11 +1,30 @@
 class github-pages {
-	package {'ruby1.9.3':
-		ensure  => installed,
-		require => Class["system-update"],
-        notify => Exec['gem install github-pages'],
+	package {'ruby':
+		ensure  => 'installed',
+		require => Class['system-update'],
+        notify => Exec['install-rvm'],
 	}
-    exec {'gem install github-pages':
-        command => 'gem install github-pages --no-ri --no-rdoc',
-        onlyif => 'dpkg -l | grep -c ruby1.9.3 | wc -l',
+    
+    exec {'gpg-signature':
+        command => 'sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3',
+        notify => Exec['install-rvm'],
+    }
+    
+    exec {'install-rvm': 
+        command => 'sudo curl -L https://get.rvm.io | bash -s stable',
+        require => [Package['curl'], Exec['gpg-signature']],
+        notify => Exec['update-ruby'],
+    }
+    
+    exec {'update-ruby':
+        command => 'sudo rvm install 2.0.0 && sudo rvm --fuzzy alias create default 2.0.0 && sudo rvm use 2.0.0',
+        onlyif => 'dpkg -l | grep -c ruby | wc -l',
+        #--latest-binary --autolibs=enabled
+    }
+    
+    package {'github-pages':
+        ensure => 'installed',
+        require => Exec['update-ruby'],
+        provider => 'gem',        
     }
 }
